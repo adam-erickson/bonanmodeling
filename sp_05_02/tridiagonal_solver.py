53ef994b81a7ae23a426d190a9a36b587e80015f
Adam Erickson <adam.michael.erickson@gmail.com>
2019-07-29 13:56:27 -0700

    update

--- sp_05_02/tridiagonal_solver.py
+++ sp_05_02/tridiagonal_solver.py
@@ -1,46 +1,52 @@
-function[u] = tridiagonal_solver(a, b, c, d, n)
+# Author: Adam Erickson, PhD, Washington State University
 
-# Solve for U given the set of equations R * U = D, where U is a vector
-# of length N, D is a vector of length N, and R is an N x N tridiagonal
-# matrix defined by the vectors A, B, C each of length N. A(1) and
-# C(N) are undefined and are not referenced.
-#
-#     |B(1) C(1) ...  ...  ...                     |
-#     |A(2) B(2) C(2) ...  ...                     |
-# R = |     A(3) B(3) C(3) ...                     |
-#     |                    ... A(N-1) B(N-1) C(N-1)|
-#     |                    ... ...    A(N)   B(N)  |
-#
-# The system of equations is written as:
-#
-#    A_i * U_i-1 + B_i * U_i + C_i * U_i+1 = D_i
-#
-# for i = 1 to N. The solution is found by rewriting the
-# equations so that:
-#
-#    U_i = F_i - E_i * U_i+1
+import numpy as np
 
-# --- Forward sweep (1 -> N) to get E and F
 
-e(1) = c(1) / b(1)
+def tridiagonal_solver(a, b, c, d, n):
+    """
+    Solve for U given the set of equations R * U = D, where U is a vector
+    of length N, D is a vector of length N, and R is an N x N tridiagonal
+    matrix defined by the vectors A, B, C each of length N. A(1) and
+    C(N) are undefined and are not referenced.
 
-for i = 2:
-    1: n-1
-    e(i) = c(i) / (b(i) - a(i) * e(i-1))
-end
+            |B(1) C(1) ...  ...  ...                     |
+            |A(2) B(2) C(2) ...  ...                     |
+        R = |     A(3) B(3) C(3) ...                     |
+            |                    ... A(N-1) B(N-1) C(N-1)|
+            |                    ... ...    A(N)   B(N)  |
 
-f(1) = d(1) / b(1)
+    The system of equations is written as:
 
-for i = 2:
-    1: n
-    f(i) = (d(i) - a(i) * f(i-1)) / (b(i) - a(i) * e(i-1))
-end
+        A_i * U_i-1 + B_i * U_i + C_i * U_i+1 = D_i
 
-# --- Backward substitution (N -> 1) to solve for U
+    for i = 1 to N. The solution is found by rewriting the equations
+    so that:
 
-u(n) = f(n)
+        U_i = F_i - E_i * U_i+1
+    """
 
-for i = n-1:
-    -1: 1
-    u(i) = f(i) - e(i) * u(i+1)
-end
+    e = np.zeros(n-1)
+    f = np.zeros(n)
+    u = np.zeros(n-1)
+
+    # --- Forward sweep (1 -> N) to get E and F
+
+    e[1] = c[1] / b[1]
+
+    for i in range(1, n-1, 1):
+        e[i] = c[i] / (b[i] - a[i] * e[i-1])
+
+    f[1] = d[1] / b[1]
+
+    for i in range(1, n, 1):
+        f[i] = (d[i] - a[i] * f[i-1]) / (b[i] - a[i] * e[i-1])
+
+    # --- Backward substitution (N -> 1) to solve for U
+
+    u[n] = f[n]
+
+    for i in range(n-1, 1, -1):
+        u[i] = f[i] - e[i] * u[i+1]
+
+    return u
